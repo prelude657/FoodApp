8cc81733ced032ab981d86ce1b1bf868f61a5ee6
Prelude657 <56809218+prelude657@users.noreply.github.com>
2021-07-20 16:49:33 -0300

    Food app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+
+  {
+    path: 'details',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  }, 
   {
     path: '',
     redirectTo: 'home',
